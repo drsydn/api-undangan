71f5018d0576d6c2158c4f1251f1c3272503b4c3
drsydn <difarosyidin8@gmail.com>
2025-05-01 13:28:19 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 /**
  * Import semua class yang digunakan dalam framework ini.
